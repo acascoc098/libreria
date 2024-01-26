01ed4bfc36ea242e3ecf97773570ba0b34f29fb9
acascoc098 <acascoc098@g.educaand.es>
2024-01-26 14:42:51 +0100

    Loading hecho.

--- src/App.js
+++ src/App.js
@@ -2,27 +2,35 @@ import logo from './logo.svg';
 import './App.css';
 import { useEffect, useState } from 'react';
 import Book from './Book';
+import { wait } from '@testing-library/user-event/dist/utils';
 
 function App() {
   const [books, setBooks] = useState([]);
+  const [loading, setLoading] = useState(true);
 
-  const downloadBooks = async () => {
+  const downloadBooks = async () => {    
+    console.log(loading);
     const response = await fetch("http://localhost:3001/books");
     const books = await response.json();
-    console.log(books);
     setBooks(books);
   }
 
   useEffect(() => {
-    downloadBooks();
+    setTimeout(() => {
+      downloadBooks();
+      setLoading(false);
+    },3000)
   },[]);
 
   return (
     <div className="App">
       {
-        books.map(book => //Sin la llaves, porque si no hya que poner un return
-          <Book book={book}/>
-        )
+        loading ? 
+          <img src='iconos/bar-progress.gif' alt='loading'/>
+        :
+          books.map(book => //Sin la llaves, porque si no hya que poner un return
+            <Book book={book} key={book.id}/>
+          )
       }
     </div>
   );
